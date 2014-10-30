b1a27705cc90a77f66ee2710f6cf3f50153506b5
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2014-10-30 22:32:35 +0100

    Añadidos nuevos estilos y diseño

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" media="screen" href="recursos/css/estilos.css"/>
+		<link rel="shortcut icon" type="image/png" href="recursos/imagenes/favicon.png">	
 		<title>BinWeb</title>
 	<head>
 	<body>
@@ -12,34 +13,38 @@
 				for($i=1;$i<=11;$i++)
 				{
 					echo "<tr>";
-						for($j=0;$j<9;$j++)
+					for($j=0;$j<9;$j++)
+					{
+						if(($i==10 && $j==0)||($i==11 && $j!=8))
+							echo "<td class='oculto'></td>";
+						else
 						{
-							if(($i==10 && $j==0)||($i==11 && $j!=8))
-								echo "<td class='oculto'></td>";
+							if($j>0)
+							{
+								$iAux=$i-1;
+								$jAux=$j;
+								if($j==8 && $i==11)
+								{
+									$iAux=0;
+									$jAux=$j+1;
+								}
+							}
 							else
 							{
-								if($j>0)
-								{
-									$iAux=$i-1;
-									$jAux=$j;
-									if($j==8 && $i==11)
-									{
-										$iAux=0;
-										$jAux=$j+1;
-									}
-								}
-								else
-								{
-									$iAux=$i;
-									$jAux="";
-								}								
-								echo "<td>".($jAux).($iAux)."</td>";
-							}
+								$iAux=$i;
+								$jAux="";
+							}								
+							echo "<td>".($jAux).($iAux)."</td>";
 						}
+					}
 					echo "</tr>";
 				}
 ?>
-			<table>
+			</table>
+			<form action="index.php" method="post">
+				<input type="submit" name="obtener" value="Obtener Número">
+				<input type="submit" name="nuevo" 	value="Nuevo Juego">
+			</form>
 		</div>
 	</body>
 </html>
