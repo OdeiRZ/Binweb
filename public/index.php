7c991ba85e263580a7e53b5f383844ea3abe5e97
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2014-10-31 02:09:18 +0100

    Añadida funcionalidad básica de generación de números

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
+<?php
+	require("recursos/inc/funciones.php");
+	$debug=false;
+	
+	session_name('loteria');
+	session_start();
+	$aleatorio=0;
+	
+	if(isset($_POST['nuevo']))
+	{
+		session_destroy();
+		session_start();
+	}
+	if(!isset($_SESSION['carton']))
+		$_SESSION['carton']=array_fill(1,90,0);
+	if(isset($_POST['obtener']))
+	{
+		$aleatorio=mt_rand(1,90);
+		$auxAleatorio=strval($aleatorio);
+		$indice=($aleatorio>9) ? obtenerIndice($auxAleatorio[1]+1,$auxAleatorio[0]) : obtenerIndice($auxAleatorio[0],"0");
+		$_SESSION['carton'][$indice]=1;
+	}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,46 +28,19 @@
 		<link rel="stylesheet" type="text/css" media="screen" href="recursos/css/estilos.css"/>
 		<link rel="shortcut icon" type="image/png" href="recursos/imagenes/favicon.png">	
 		<title>BinWeb</title>
-	<head>
+	</head>
 	<body>
 		<div>
-			<table>
 <?php
-				for($i=1;$i<=11;$i++)
-				{
-					echo "<tr>";
-					for($j=0;$j<9;$j++)
-					{
-						if(($i==10 && $j==0)||($i==11 && $j!=8))
-							echo "<td class='oculto'></td>";
-						else
-						{
-							if($j>0)
-							{
-								$iAux=$i-1;
-								$jAux=$j;
-								if($j==8 && $i==11)
-								{
-									$iAux=0;
-									$jAux=$j+1;
-								}
-							}
-							else
-							{
-								$iAux=$i;
-								$jAux="";
-							}								
-							echo "<td>".($jAux).($iAux)."</td>";
-						}
-					}
-					echo "</tr>";
-				}
+			$auxAleatorio=($aleatorio!=0)? $aleatorio :"";
+			echo "\t\t\t<p><span>".$auxAleatorio."</span></p>\n";
+			echo dibujaTabla();
 ?>
-			</table>
 			<form action="index.php" method="post">
 				<input type="submit" name="obtener" value="Obtener Número">
-				<input type="submit" name="nuevo" 	value="Nuevo Juego">
+				<input type="submit" name="nuevo"	value="Nuevo Juego">
 			</form>
 		</div>
+		<?= isDebug($debug); ?>
 	</body>
 </html>
---
+++ public/recursos/inc/funciones.php
@@ -0,0 +1,51 @@
+<?php
+	function dibujaTabla()
+	{
+		$tabla="";
+		for($i=1;$i<=11;$i++)
+		{
+			$tabla.="\t\t\t<table>\n\t\t\t\t<tr>\n";
+			for($j=0;$j<9;$j++)
+			{
+				if(($i==10 && $j==0)||($i==11 && $j!=8))
+					$tabla.="\t\t\t\t\t<td class='oculto'></td>\n";
+				else						
+					$tabla.="\t\t\t\t\t<td>".obtenerIndice($i,$j)."</td>\n";
+			}
+			$tabla.="\t\t\t\t</tr>\n\t\t\t</table>\n";
+		}
+		return $tabla;
+	}
+	function obtenerIndice($i,$j)
+	{
+		if($j>0)
+		{
+			$iAux=$i-1;
+			$jAux=$j;
+			if($j==8 && $i==11)
+			{
+				$iAux=0;
+				$jAux=$j+1;
+			}
+		}
+		else
+		{
+			$iAux=$i;
+			$jAux="";
+		}
+		return ($jAux).($iAux);
+	}
+	function isDebug($debug)
+	{
+		if($debug)
+			for($i=1;$i<=11;$i++)
+			{
+				for($j=0;$j<9;$j++)
+					if(($i==10 && $j==0)||($i==11 && $j!=8))
+						echo "&nbsp; &nbsp;";
+					else						
+						echo $_SESSION['carton'][obtenerIndice($i,$j)]." ";
+				echo "<br/>";
+			}
+	}
+?>
