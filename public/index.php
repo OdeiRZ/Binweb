486bc121ec573107b495eee16068355d90031978
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2014-11-02 16:46:42 +0100

    Aplicado estándar PSR-2 a código fuente

--- public/index.php
+++ public/index.php
@@ -1,77 +1,85 @@
 <?php
-	require("recursos/inc/funciones.php");
-	$debug=false;
-	$aleatorio=0;
-	$titulo="Bingo Online";
-	
-	session_name('loteria');
-	session_start();
-	
-	if(isset($_POST['nuevo']))
-	{
-		session_destroy();
-		session_start();
+    require("recursos/inc/funciones.php");
+    
+    $n=0;
+    $debug=true;
+    $titulo="Bingo Online";
+    
+    session_name('loteria');
+    session_start();
+    
+    if (isset($_POST['nuevo'])) {
+        session_destroy();
+        session_start();
+    }
+    if (!isset($_SESSION['tombola'])) {
+        $_SESSION['tombola']=array_fill(1, 90, 0);
 	}
-	if(!isset($_SESSION['tombola']))
-		$_SESSION['tombola']=array_fill(1,90,0);
-	if(isset($_POST['obtener']))
-	{
-		do{
-			$aleatorio=mt_rand(1,90);
-			$auxAleatorio=strval($aleatorio);
-			$indice=($aleatorio>9) ? obtenerIndice($auxAleatorio[1]+1,$auxAleatorio[0]) : obtenerIndice($auxAleatorio[0],0);
-		}while($_SESSION['tombola'][$indice]!=0);
-		$_SESSION['tombola'][$indice]=$aleatorio;
-	}
-	if(isset($_POST['comprobar']))
-	{
-		$carton=array_fill(1,27,0);
-		$fila1=array();
-		$fila2=array();
-		$fila3=array();
-		for($i=1;$i<4;$i++)
-			for($j=0;$j<9;$j++)
-			{
-				$indice=($i+$j*3);
-				if($_POST["casilla".$indice]!="")
-				{	//alternativa => funcion de php in_array();
-					$carton[$indice]=$_POST["casilla".$indice];
-					($indice==1||$indice==4||$indice==7||$indice==10||$indice==13||$indice==16||$indice==19||$indice==22||$indice==25) ? array_push($fila1,$carton[$indice]) : "";
-					($indice==2||$indice==5||$indice==8||$indice==11||$indice==14||$indice==17||$indice==20||$indice==23||$indice==26) ? array_push($fila2,$carton[$indice]) : "";
-					($indice==5||$indice==6||$indice==9||$indice==12||$indice==15||$indice==18||$indice==21||$indice==24||$indice==27) ? array_push($fila3,$carton[$indice]) : "";
-				}
+    if (isset($_POST['obtener'])) {
+        do {
+            $n=mt_rand(1, 90);
+            $nAux=strval($n);
+            if ($n>9) {
+                $pos=obtenerIndice($nAux[1]+1, $nAux[0]);
+			} else {
+                $pos=obtenerIndice($nAux[0], 0);
 			}
-		if(count(array_keys($carton,0))!=12)
-			$titulo="<font color='red'>Cartón no rellenado</font>";
-		else
-			$titulo=comprobarCarton($carton,$fila1,$fila2,$fila3,$_SESSION['tombola']);
-	}
+        } while ($_SESSION['tombola'][$pos]!=0);
+        $_SESSION['tombola'][$pos]=$n;
+    }
+    if (isset($_POST['comprobar'])) {
+        $carton=array_fill(1, 27, 0);
+        $f1=array();
+        $f2=array();
+        $f3=array();
+        for($i=1;$i<4;$i++) {
+            for($j=0;$j<9;$j++) {
+                $pos=($i+$j*3);
+                if ($_POST["casilla".$pos]!="") {
+                    $carton[$pos]=$_POST["casilla".$pos];
+                    if (in_array($pos, array(1, 4, 7, 10, 13, 16, 19, 22, 25))) {
+                        array_push($f1,$carton[$pos]);
+					}  else if (in_array($pos, array(2, 5, 8, 11, 14, 17, 20, 23, 26))) {
+                        array_push($f2, $carton[$pos]);
+                    } else {
+                        array_push($f3, $carton[$pos]);
+					}
+                }
+            }
+		}
+        if(count(array_keys($carton, 0))!=12) {
+            $titulo="<font color='red'>Cartón no rellenado</font>";
+        } else {
+            $titulo=comprobarCarton($carton, $f1, $f2, $f3, $_SESSION['tombola']);
+		}
+    }
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" media="screen" href="recursos/css/estilos.css"/>
-		<link rel="shortcut icon" type="image/png" href="recursos/imagenes/favicon.png">	
-		<title>BinWeb</title>
-	</head>
-	<body>
-		<form action="index.php" method="post">
-			<div>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="recursos/css/estilos.css"/>
+        <link rel="shortcut icon" type="image/png" href="recursos/imagenes/ico.png">
+        <title>BinWeb</title>
+    </head>
+    <body>
+        <form action="index.php" method="post">
+            <div>
 <?php
-				$auxAleatorio=($aleatorio!=0)? $aleatorio :"";
-				echo "\t\t\t\t<p><span>".$auxAleatorio."</span><span>".$titulo."</span></p>\n";
-				echo dibujaTabla($aleatorio,$_SESSION['tombola']);
+                $nAux=($n!=0) ? $n : "";
+                echo "\t\t\t\t<p><span>".$nAux."</span><span>".$titulo."</span></p>\n";
+                echo dibujaTabla($n, $_SESSION['tombola']);
+                $activo=(count(array_keys($_SESSION['tombola'], 0))==0) ? "disabled" : "";
 ?>
-				<input type="submit" name="obtener" value="Obtener Número" <?= (count(array_keys($_SESSION['tombola'],0))==0) ? "disabled" : ""; ?>>
-				<input type="submit" name="nuevo"	value="Nuevo Juego">
-			</div>
-			<div>
+                <input type="submit" name="obtener" value="Obtener Número" <?= $activo ?>>
+                <input type="submit" name="nuevo"    value="Nuevo Juego">
+            </div>
+            <div>
 <?= dibujaBoleto(); ?>
-				<input type="submit" name="comprobar" value="Comprobar Cartón">
-			</div>
-		</form>
+                <input type="submit" name="comprobar" value="Comprobar Cartón">
+            </div>
+        </form>
 <?= isDebug($debug); ?>
 <?= reproducirAudio($titulo); ?>
-	</body>
+    </body>
 </html>
--- public/recursos/inc/funciones.php
+++ public/recursos/inc/funciones.php
@@ -1,120 +1,116 @@
 <?php
-	function dibujaTabla($actual,$tombola)
-	{
-		$tabla="\t\t\t\t<table>\n";
-		for($i=1;$i<12;$i++)
-		{
-			$tabla.="\t\t\t\t\t<tr>\n";
-			for($j=0;$j<9;$j++)
-			{
-				$estilo="";
-				$indice=obtenerIndice($i,$j);
-				if(($i==10 && $j==0)||($i==11 && $j!=8))
-					$estilo="oculto";
-				else if($indice!=$actual && $tombola[$indice]!=0)
-					$estilo="boleto";
-				else if($indice==$actual)
-					$estilo="actual";
-				$tabla.="\t\t\t\t\t\t<td class='".$estilo."'>".$indice."</td>\n";
-			}
-			$tabla.="\t\t\t\t\t</tr>\n";
-		}
-		return $tabla."\t\t\t\t</table>\n";
-	}
-	function dibujaBoleto()
-	{
-		$boleto="\t\t\t\t<table>\n";
-		for($i=1;$i<4;$i++)
-		{
-			$boleto.="\t\t\t\t\t<tr>\n";
-			for($j=0;$j<9;$j++)
-			{
-				$min=($j==0)? 1 : $j*10;
-				$max=($j==0)? 9 : $min+9;
-				$max=($j==8)? 90: $max;
-				$boleto.="\t\t\t\t\t\t<td><input type='number' name='casilla".($i+$j*3)."' min='".$min."' max='".$max."'></td>\n";
-			}
-			$boleto.="\t\t\t\t\t</tr>\n";
-		}
-		return $boleto."\t\t\t\t</table>\n";
-	}
-	function obtenerIndice($i,$j)		//reinsertar funcion en funcion padre en version final
-	{
-		if($j>0)
-		{
-			$iAux=$i-1;
-			$jAux=$j;
-			if($j==8 && $i==11)
-			{
-				$iAux=0;
-				$jAux=$j+1;
-			}
-		}
-		else
-		{
-			$iAux=$i;
-			$jAux="";
-		}
-		return ($jAux).($iAux);
-	}
-	function comprobarCarton($carton,$fila1,$fila2,$fila3,$tombola)
-	{
-		if(count(array_diff($carton, $tombola)) === 0)
-			$resultado="<font color='green'>!!!! BINGO !!!!</font>";
-		else if((count(array_diff($fila1, $tombola)) === 0 && count($fila1)>=5) || (count(array_diff($fila2, $tombola)) === 0 && count($fila2)>=5) || (count(array_diff($carton, $fila3)) === 0 && count($fila3)>=5))
-			$resultado="<font color='green'>!! LÍNEA !!</font>";
-		else
-			$resultado="<font color='red'>Sin Premio</font>";
-		return $resultado;
-	}
-	function reproducirAudio($titulo)
-	{
-		$sw=true;
-		if(strpos($titulo,"BINGO") || strpos($titulo,"LÍNEA"))
-			$audio="win";
-		else if(strpos($titulo,"Sin Premio"))
-			$audio="fail";
-		else
-			$sw=false;
-		
-		if($sw)
-		{
-			$reproductor="\t\t<audio autoplay>\n";
-			$reproductor.="\t\t\t<source src='/recursos/audio/".$audio.".mp3' type='audio/mpeg'>\n";
-			$reproductor.="\t\t\t<source src='/recursos/audio/".$audio.".ogg' type='audio/ogg'>\n";
-			$reproductor.="\t\t</audio>\n";
-			echo $reproductor;
-		}
-	}
-	function isDebug($debug)			//eliminar funcion en version final
-	{
-		if($debug)
-		{
-			for($i=1;$i<12;$i++)
-			{
-				for($j=0;$j<9;$j++)
-					if(($i==10 && $j==0)||($i==11 && $j!=8))
-						echo "&nbsp; &nbsp;";
-					else if($_SESSION['tombola'][obtenerIndice($i,$j)]!=0)
-						echo "1 ";
-					else
-						echo "0 ";
-				echo "<br/>";
-			}
-			echo "<br/>Contador de Números Tómbola: ".count(array_keys($_SESSION['tombola'],1))."</br>";
-			echo "Contador de Huecos Tómbola: ".count(array_keys($_SESSION['tombola'],0))."<br/><br/>";
-			if(isset($_POST['comprobar']))
-			{
-				for($i=1;$i<4;$i++)
-				{
-					for($j=0;$j<9;$j++)
-						if($_POST["casilla".($i+$j*3)]=="")
-							echo "0 ";
-						else
-							echo "1 ";
-					echo "<br/>";
+    function dibujaTabla($actual,$tombola)
+    {
+        $tabla="\t\t\t\t<table>\n";
+        for ($i=1;$i<12;$i++) {
+            $tabla.="\t\t\t\t\t<tr>\n";
+            for ($j=0;$j<9;$j++) {
+                $estilo="";
+                $pos=obtenerIndice($i, $j);
+                if (($i==10 && $j==0) || ($i==11 && $j!=8)) {
+                    $estilo="oculto";
+				} else if ($pos!=$actual && $tombola[$pos]!=0) {
+                    $estilo="boleto";
+				} else if ($pos==$actual) {
+                    $estilo="actual";
 				}
-			}
+                $tabla.="\t\t\t\t\t\t<td class='".$estilo."'>".$pos."</td>\n";
+            }
+            $tabla.="\t\t\t\t\t</tr>\n";
+        }
+        return $tabla."\t\t\t\t</table>\n";
+    }
+    function dibujaBoleto()
+    {
+        $boleto="\t\t\t\t<table>\n";
+        for ($i=1;$i<4;$i++) {
+            $boleto.="\t\t\t\t\t<tr>\n";
+            for ($j=0;$j<9;$j++) {
+                $minAux=($j==0) ? 1 : $j*10;
+                $min="min='".$minAux."'";
+                $maxAux=($j==0) ? 9 : $minAux+9;
+                $max="max='".(($j==8) ? 90 : $maxAux)."'";
+                $name="name='casilla".($i+$j*3)."'";
+                $boleto.="\t\t\t\t\t\t<td><input type='number' ".$name." ".$min." ".$max."></td>\n";
+            }
+            $boleto.="\t\t\t\t\t</tr>\n";
+        }
+        return $boleto."\t\t\t\t</table>\n";
+    }
+    function obtenerIndice($i,$j)        //reinsertar funcion en funcion padre en version final
+    {
+        if ($j>0) {
+            $iAux=$i-1;
+            $jAux=$j;
+            if ($j==8 && $i==11) {
+                $iAux=0;
+                $jAux=$j+1;
+            }
+        } else {
+            $iAux=$i;
+            $jAux="";
+        }
+        return ($jAux).($iAux);
+    }
+    function comprobarCarton($carton,$fila1,$fila2,$fila3,$tombola)	//
+    {
+        if (count(array_diff($carton, $tombola)) === 0) {
+            $resultado="<font color='green'>!!!! BINGO !!!!</font>";
+        } else if ((count(array_diff($fila1, $tombola)) === 0 && count($fila1)>=5) || 
+                   (count(array_diff($fila2, $tombola)) === 0 && count($fila2)>=5) || 
+                   (count(array_diff($fila3, $tombola)) === 0 && count($fila3)>=5)) {
+            $resultado="<font color='green'>!! LÍNEA !!</font>";
+		} else {
+            $resultado="<font color='red'>Sin Premio</font>";
 		}
-	}
-?>
+        return $resultado;
+    }
+    function reproducirAudio($titulo)
+    {
+        $sw=true;
+        if (strpos($titulo, "BINGO") || strpos($titulo, "LÍNEA")) {
+            $audio="win";
+		} elseif (strpos($titulo, "Sin Premio")) {
+            $audio="fail";
+		} else {
+            $sw=false;
+		}
+        if ($sw) {
+            $reproductor="\t\t<audio autoplay>\n";
+            $reproductor.="\t\t\t<source src='/recursos/audio/".$audio.".mp3' type='audio/mpeg'>\n";
+            $reproductor.="\t\t\t<source src='/recursos/audio/".$audio.".ogg' type='audio/ogg'>\n";
+            $reproductor.="\t\t</audio>\n";
+            return $reproductor;
+        }
+    }
+    function isDebug($debug)            //eliminar funcion en version final
+    {
+        if ($debug) {
+            for ($i=1;$i<12;$i++) {
+                for ($j=0;$j<9;$j++) {
+                    if (($i==10 && $j==0)||($i==11 && $j!=8)) {
+                        echo "&nbsp; &nbsp;";
+					} else if ($_SESSION['tombola'][obtenerIndice($i, $j)]!=0) {
+                        echo "1 ";
+					} else {
+                        echo "0 ";
+					}
+				}
+                echo "<br/>";
+            }
+            echo "<br/>Contador de Números Tómbola: ".count(array_keys($_SESSION['tombola'], 1))."</br>";
+            echo "Contador de Huecos Tómbola: ".count(array_keys($_SESSION['tombola'], 0))."<br/><br/>";
+            if (isset($_POST['comprobar'])) {
+                for ($i=1;$i<4;$i++) {
+                    for ($j=0;$j<9;$j++) {
+                        if ($_POST["casilla".($i+$j*3)]=="") {
+                            echo "0 ";
+						} else {
+                            echo "1 ";
+						}
+					}
+                    echo "<br/>";
+                }
+            }
+        }
+    }
